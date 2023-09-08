91d6ed5c31bd33c9483c7a9299005f04ed6ad897
iamonkara <iamonkara@gmail.com>
2023-09-08 12:49:42 -0500

    HasOne association cannot be polymorphic
    if an association does not respond to `polymorphic?` that should not throw errors as the main purpose of deleting the records recursively is interrupted

--- lib/delete_recursively/associated_class_finder.rb
+++ lib/delete_recursively/associated_class_finder.rb
@@ -16,7 +16,7 @@ module DeleteRecursively::AssociatedClassFinder
 
     def find_classes(reflection)
       result =
-        if reflection.polymorphic?
+        if reflection.polymorphic? rescue nil
           find_classes_for_polymorphic_reflection(reflection)
         else
           [reflection.klass]
